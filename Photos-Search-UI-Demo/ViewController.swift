40a93278d23cc927f5afbe20feaf8cbdaf8dd1e0
sebjvidal <seb.vidal@btinternet.com>
2024-06-22 15:33:59 +0100

    Initial Commit

---
+++ Photos-Search-UI-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photos-Search-UI-Demo
+//
+//  Created by Seb Vidal on 22/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
